e4134851a5819a10a16fd1f20595243cbeb1f8e3
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2023-10-04 15:00:39 +0300

    Added method that gets members info of a data type

--- Reflection_WorkingWithMetadata/Program.cs
+++ Reflection_WorkingWithMetadata/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Reflection_WorkingWithMetadata
 {
-    public static class Program
+    internal class Program
     {
+        public static void GetMembersInfo(string stringType)
+        {
+            Type type = Type.GetType(stringType);
+            var members = type.GetMembers(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (MemberInfo memberInfo in members)
+            {
+                Console.WriteLine(memberInfo.Name);
+            }
+            Console.ReadLine();
+        }
         public static void Main(string [] args)
         {
-            Console.WriteLine("Hello World !");
+            string stringType = "Reflection_WorkingWithMetadata.Student";
+            GetMembersInfo(stringType);
         }
     }
 }

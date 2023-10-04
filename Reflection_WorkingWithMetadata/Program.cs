f45b3ed9cb690e5751c2c60989800498cf8b63c0
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2023-10-04 13:15:18 +0300

    First commit. Added class Student to learn work with metadata and reflection

---
+++ Reflection_WorkingWithMetadata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reflection_WorkingWithMetadata
+{
+    public static class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+    }
+}
